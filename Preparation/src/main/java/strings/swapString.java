92ec6c3e0a202b86a3d921d9fa8c835842f96f93
RijwanaShaik <124487541+RijwanaShaik@users.noreply.github.com>
2024-08-11 19:54:14 +0530

    java preparation

---
+++ Preparation/src/main/java/strings/swapString.java
@@ -0,0 +1,16 @@
+package strings;
+
+public class swapString {
+    public static void main(String[] args) {
+        String s1="java";
+        String s2="program";
+        s1 = s1 + s2;
+        System.out.println(s1);
+        s2 = s1.substring(0,s1.length() - s2.length());
+        System.out.println(s2.length());//s1 starts reading index from 4 to end of s1 length
+        s1 = s1.substring(s2.length());
+
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}
